8e019da5704029d85b1f2b0176e1854f95b3827d
Marc Risney <mrisney@redhat.com>
2018-08-19 17:08:12 -0700

    initial commit

---
+++ BarcodeReader/BarcodeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BarcodeViewController.swift
+//  BarcodeReader
+//
+//  Created by Marc Risney on 18/08/2018.
+//
+
+import UIKit
+
+class BarcodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Drivers Licene Barcode Reader in Swift
+
+This is an app that scans the PDF417 bar code on the back of a US driver's icense
+and uploads the information to a REST services.
